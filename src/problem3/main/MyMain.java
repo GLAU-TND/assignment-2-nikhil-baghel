8cf28535b9978ed4c439d5129169e429d331beba
nikhilnaghel <nikhil.baghel.32@gmail.com>
2020-03-31 22:11:24 +0530

    created data ,left and right field

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,14 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("samar", 1);
+        obj.insert("sameer", 4);
+        obj.insert("sudheer", 3);
+        obj.insert("samrat", 2);
+        obj.insert("surendra", 5);
+        obj.display();
+    }
 }
--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,4 +7,23 @@
 package problem5.node;
 // to define node properties
 public class Node {
+
+    Student student;
+    Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
